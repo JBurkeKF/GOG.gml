17b2a3f0291d4b540a25c146f34deb69c8c99458
Joel Burke <jburke@kittehface.com>
2018-12-11 00:12:00 -0600

    Fixed the library rpath so libGalaxy can be found when actually running the game.

--- build-osx.sh
+++ build-osx.sh
@@ -4,8 +4,8 @@ mkdir -p GOG.gml/x64
 mkdir -p GOG.gml.gmx/extensions/GOG.gml/x86
 mkdir -p GOG.gml.gmx/extensions/GOG.gml/x64
 g++ -m32 -O3 -fPIC -g -c -o GOG.gml/x86/GOG.gml.o GOG.gml/GOG.gml.cpp -std=c++11
-g++ -m32 GOG.gml/x86/GOG.gml.o -shared -o GOG.gml.gmx/extensions/GOG.gml/x86/GOG.gml.dylib -Wl,-rpath,assets/ -LGOG/Libraries -lgalaxy
+g++ -m32 GOG.gml/x86/GOG.gml.o -shared -o GOG.gml.gmx/extensions/GOG.gml/x86/GOG.gml.dylib -Wl,-rpath,../Resources/ -LGOG/Libraries -lgalaxy
 g++ -m64 -O3 -fPIC -g -c -o GOG.gml/x64/GOG.gml.o GOG.gml/GOG.gml.cpp -std=c++11
-g++ -m64 GOG.gml/x64/GOG.gml.o -shared -o GOG.gml.gmx/extensions/GOG.gml/x64/GOG.gml.dylib -Wl,-rpath,assets/ -LGOG/Libraries -lgalaxy
+g++ -m64 GOG.gml/x64/GOG.gml.o -shared -o GOG.gml.gmx/extensions/GOG.gml/x64/GOG.gml.dylib -Wl,-rpath,../Resources/ -LGOG/Libraries -lgalaxy
 lipo -create GOG.gml.gmx/extensions/GOG.gml/x86/GOG.gml.dylib GOG.gml.gmx/extensions/GOG.gml/x64/GOG.gml.dylib -output GOG.gml.gmx/extensions/GOG.gml/GOG.gml.dylib
 cp GOG/Libraries/libGalaxy.dylib GOG.gml.gmx/extensions/GOG.gml/libGalaxy.dylib
